3ddf3a66ec2f658cde9f9effc97fe8b5dd2e2ac4
mannsvijay <mnvj69@gmail.com>
2026-03-02 21:30:02 +0530

    imported tailwind

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -7,9 +7,14 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold text-red-500"> Taikwind test || </h1>
-    </>
+   <button className="relative block group ">
+      <span className="absolute inset-0 border-2 border-white border-dashed rounded-lg"></span>
+      <div className="transition bg-black border-2 border-white rounded-lg group-hover:-translate-x-2 group-hover:-translate-y-2">
+        <div className="p-2 ">
+          <p className="text-xl font-outerSans font-medium">Button</p>
+        </div>
+      </div>
+    </button>
   )
 }
 
