52d613e5a4fd0fec361601452baf664ac54b5757
mannsvijay <mnvj69@gmail.com>
2026-03-02 21:49:51 +0530

    fixed some bugs

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/button'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <button className="relative block group ">
-      <span className="absolute inset-0 border-2 border-white border-dashed rounded-lg"></span>
-      <div className="transition bg-black border-2 border-white rounded-lg group-hover:-translate-x-2 group-hover:-translate-y-2">
-        <div className="p-2 ">
-          <p className="text-xl font-outerSans font-medium">Button</p>
-        </div>
-      </div>
-    </button>
+    <>
+      <Button />
+    </>
   )
 }
 
