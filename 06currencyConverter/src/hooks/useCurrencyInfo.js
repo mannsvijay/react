1d9c27d82482b9ff7e32fe96200f213accae91f5
mannsvijay <mnvj69@gmail.com>
2026-03-09 16:08:27 +0530

    api call for currency

---
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,54 @@
+import { useState , useEffect} from "react";
+
+function useCurrencyInfo(currency) {
+     const  [data, setData] = useState({});
+     useEffect(()=>{
+          fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`)
+          .then(res=>res.json())
+          .then((res)=> setData(res[currency]))
+
+     },[])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       setLoading(true);
+//       try {
+//         const response = await fetch(`https://api.exchangerate-api.com/v4/latest/${currency}`);
+//         const result = await response.json();
+//         setData(result);
+//       } catch (error) {
+//         console.error("Error fetching currency info:", error);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     if (currency) {
+//       fetchData();
+//     }
+//   }, [currency]);
+
+//   return { data, loading };
