75af8b50d28380d3d238e82c09dad43f494fbc39
mannsvijay <mnvj69@gmail.com>
2026-03-12 20:45:00 +0530

    adding more elements and fixed some bugs

--- 07Webpage/src/components/Contact/Contact.jsx
+++ 07Webpage/src/components/Contact/Contact.jsx
@@ -38,7 +38,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    Acme Inc, Street, State, Postal Code
+                                    plot no 5 prajapat nagar new sanganer road mansarovar jaipur 
                                 </div>
                             </div>
 
@@ -60,7 +60,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    +44 1234567890
+                                    +91 9602243295
                                 </div>
                             </div>
 
@@ -82,7 +82,7 @@ export default function Contact() {
                                     />
                                 </svg>
                                 <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    info@acme.org
+                                    mananvijay13@gmail.com
                                 </div>
                             </div>
                         </div>
--- 07Webpage/src/main.jsx
+++ 07Webpage/src/main.jsx
@@ -6,13 +6,7 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
-
+import Contact from './components/Contact/Contact.jsx'
 
 
 const router = createBrowserRouter([
@@ -27,7 +21,20 @@ const router = createBrowserRouter([
         {
           path : "about",
           element : <About />
+        },
+        {
+          path : "contact",
+          element : <Contact />
         }
+
       ]
     }
 ])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>,
+)
+
+
