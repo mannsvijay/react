67eb478d7aa502aed7be66b7f2fabf4a6b717388
mannsvijay <mnvj69@gmail.com>
2026-03-15 23:48:57 +0530

    created userContext successfully

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import UserContextProvider from './context/UserContextProvider'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1> manan vijay</h1>
-    </>
+    <UserContextProvider>
+      <h1> Manan Vijay</h1>
+    </UserContextProvider>
   )
 }
 
