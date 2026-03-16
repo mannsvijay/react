8e2932049a1fe609e6f0757dec905b8504a0ef49
mannsvijay <mnvj69@gmail.com>
2026-03-17 01:27:44 +0530

    creating login credentials

--- 08miniContext/src/components/Login.jsx
+++ 08miniContext/src/components/Login.jsx
@@ -2,14 +2,18 @@ import React, {useContext,useState} from 'react'
 import UserContext from '../context/UserContext'
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+
     const handleSubmit = ()=>{
 
     }
   return (
     <div>
-      <h2> Login </h2>
-      <input type='text' placeholder='username' />
-      <input type='password' placeholder='password' />
+      <h2> Login </h2> 
+      <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} placeholder='username' />
+      <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' />
       <button onClick={handleSubmit}> Submit </button>
     </div>
   )
