7e863d0b81b5012c85bc7548c00226ab73ef3713
mannsvijay <mnvj69@gmail.com>
2026-03-17 13:11:20 +0530

    created function which changes theme

--- 09themeSwitcher/src/App.jsx
+++ 09themeSwitcher/src/App.jsx
@@ -1,10 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useEffectEvent, useState } from 'react'
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
 
 function App() {
+ const [themeMode,setThemeMode] = useState("light")
+
+ const lightTheme = () =>{
+  setThemeMode("light");
+ }
+
+ const darkTheme = () =>{
+  setThemeMode("dark");
+ }
+
+ useEffect(() => {
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+ }, [themeMode])
  
+
   return (
-    
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
           <div className="w-full">
               <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
@@ -16,7 +32,7 @@ function App() {
               </div>
           </div>
       </div>
-      
+      </ThemeProvider>
   )
 }
 
