cb7214b8cdeafcac20b213bc3f745d72e603735c
mannsvijay <mnvj69@gmail.com>
2026-03-21 18:14:54 +0530

    fixed all of the bugs and its now working with all functions now we will just update  its ui and components

--- 11reduxTodo/src/components/AddTodo.jsx
+++ 11reduxTodo/src/components/AddTodo.jsx
@@ -1,20 +1,19 @@
-import {React , useState} from 'react'
-import { useDispatch } from 'react-redux'
-import { addTodo } from '../features/todo/todoSlice'
-
+import React, {useState} from 'react'
+import {useDispatch} from 'react-redux'
+import {addTodo} from '../features/todo/todoSlice' 
 
 function AddTodo() {
-    
-    const [input,setinput] = useState('')
+
+    const [input, setInput] = useState('')
     const dispatch = useDispatch()
 
     const addTodoHandler = (e) => {
-        e.preventDefault;
-        dispatch(addTodo(input ))
-        setinput('')
+        e.preventDefault()
+        dispatch(addTodo(input))
+        setInput('')
     }
 
- return (
+  return (
     <form onSubmit={addTodoHandler} className="space-x-3 mt-12">
       <input
         type="text"
--- 11reduxTodo/src/components/Todos.jsx
+++ 11reduxTodo/src/components/Todos.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { useSelector,useDispatch } from 'react-redux'
-import { removeTodo } from '../features/todo/todoSlice'
-
+import { useSelector, useDispatch } from 'react-redux'
+import {removeTodo} from '../features/todo/todoSlice'
 
 function Todos() {
     const todos = useSelector(state => state.todos)
     const dispatch = useDispatch()
+
   return (
     <>
     <div>Todos</div>
@@ -41,6 +41,5 @@ function Todos() {
     </>
   )
 }
-
 
 export default Todos
